e7d257f9f811d91d8e8f01793329c62663bccec8
Brett LoCicero <brettlocicero@gmail.com>
2022-09-17 15:14:36 -0400

    added shop room

--- Assets/Scripts/ChunkManager.cs
+++ Assets/Scripts/ChunkManager.cs
@@ -6,10 +6,16 @@ public class ChunkManager : MonoBehaviour
 {
     public static ChunkManager instance;
 
-    [SerializeField] Chunk[] chunks;
-    [SerializeField] Path[] connectors;
+    [Header("Runtime")]
+    [SerializeField] int regionIndex;
+    [SerializeField] int roomCount;
     [SerializeField] Transform connectorSpawnPoint;
     [SerializeField] Chunk mostRecentChunk;
+
+    [Header("")]
+    [SerializeField] Region[] regions;
+
+    bool lastWasShop;
 
     void Awake () 
     {
@@ -18,16 +24,59 @@ public class ChunkManager : MonoBehaviour
 
     public void SpawnChunk ()
     {
-        Chunk chunk = chunks[Random.Range(0, chunks.Length)];
-        GameObject chunkObj = Instantiate(chunk.gameObject, connectorSpawnPoint.position, Quaternion.identity);
+        Chunk chunk;
+        GameObject chunkObj;
+
+        // spawn boss
+        if (roomCount % 5 == 0) 
+        {
+            chunk = regions[regionIndex].chunks[Random.Range(0, regions[regionIndex].chunks.Length)];
+            chunkObj = Instantiate(chunk.gameObject, connectorSpawnPoint.position, Quaternion.identity);
+        }
+
+        // spawn shop / healing
+        else if ((roomCount + 1) % 5 == 0) 
+        {
+            // spawn shop
+            if (!lastWasShop) 
+            {
+                chunk = regions[regionIndex].shopChunks[Random.Range(0, regions[regionIndex].shopChunks.Length)];
+                chunkObj = Instantiate(chunk.gameObject, connectorSpawnPoint.position, Quaternion.identity);
+            }
+
+            // spawn healing room
+            else 
+            {
+                chunk = regions[regionIndex].shopChunks[Random.Range(0, regions[regionIndex].shopChunks.Length)];
+                chunkObj = Instantiate(chunk.gameObject, connectorSpawnPoint.position, Quaternion.identity);
+            }
+        }
+
+        // spawn normal room
+        else 
+        {
+            chunk = regions[regionIndex].chunks[Random.Range(0, regions[regionIndex].chunks.Length)];
+            chunkObj = Instantiate(chunk.gameObject, connectorSpawnPoint.position, Quaternion.identity);
+        }
+
         mostRecentChunk = chunkObj.GetComponent<Chunk>();
         SpawnConnector();
+        roomCount++;
     }
 
     void SpawnConnector () 
     {
-        Path path = connectors[Random.Range(0, connectors.Length)];
+        Path path = regions[regionIndex].connectors[Random.Range(0, regions[regionIndex].connectors.Length)];
         GameObject pathObj = Instantiate(path.gameObject, mostRecentChunk.connectorSpawnPoint.position, Quaternion.identity);
         connectorSpawnPoint = pathObj.GetComponent<Path>().connectorSpawnPoint;
     }
+}
+
+[System.Serializable]
+struct Region 
+{
+    public string regionName;
+    public Chunk[] chunks;
+    public Chunk[] shopChunks;
+    public Path[] connectors;
 }
